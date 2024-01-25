d935a606ff2965f7c52f8d29fbd2842f8ba15caf
Dussan-Mamani <dussanmamani0@gmail.com>
2024-01-25 11:07:37 +0100

    subiendo

--- agregar.js
+++
@@ -1,8 +0,0 @@
-function agregarTarea(){
-    var tarea = document.getElementById("nueva-tarea").value;
-    var nuevoItem = document.createElement("li");
-    nuevoItem.textContent=tarea;
-    document.getElementById("tarea-lista").appendChild(nuevoItem);
-    document.getElementById("nueva-tarea").value=" ";
-}
-
